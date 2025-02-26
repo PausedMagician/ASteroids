053fe2a5432c03ec7bd6fc215ea8d141f63f13a5
PausedMagician <75096283+PausedMagician@users.noreply.github.com>
2025-02-26 11:58:53 +0100

    Initial

---
+++ core/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module Core {
+    requires javafx.controls;
+    opens PawzdCO.main to javafx.graphics;
+    // uses dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+    // uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+    // uses dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
+}
+
+
