e6b7f5527e0ac8d5b5f534601ccb27568507537f
PausedMagician <75096283+PausedMagician@users.noreply.github.com>
2025-06-01 02:27:35 +0200

    spacing inconsistency

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -6,7 +6,7 @@ module Core {
     // opens PawzdCO.main to javafx.graphics.spring.core;
 
 
-opens PawzdCO.main to javafx.graphics;
+    opens PawzdCO.main to javafx.graphics;
     requires common;
     uses IGamePlugin;
     uses IEntityProcessingService;
